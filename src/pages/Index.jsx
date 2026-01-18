5967505975987700f9fe1d2d50044b1def30c7fd
Akshaykumar110904 <ravulakariakshaykumar@gmail.com>
2026-01-18 20:34:15 +0530

    added recruiters,students,admin folders

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useNavigate} from 'react-router-dom'
 const Index = () => {
+  const navigate = useNavigate();
   return (
     <>
-    <div className='flex justify-center items-center bg-blue-700 min-h-screen'>
-        <div className="text-2xl font-bold text-white">
-          Hello world
+    <div className='flex justify-center items-center min-h-screen bg-gray-300'>
+        <div className="flex flex-col justify-center items-center gap-5h-75 w-75 bg-gray-200">
+          <div>CareerVortex</div>
+          <div className='flex gap-5'>
+            <button onClick={navigate("/student/login")}>Student</button>
+            <button onClick={navigate("/recruiter/login")}>Recruiter</button>
           </div>
+        </div>
     </div>
     </>
   )
--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Index from "../pages/Index.jsx"
+import StudentLogin from "../pages/Students/StudentLogin.jsx"
+import RecruiterLogin from "../pages/Recruiters/RecruiterLogin.jsx"
 const MainRouter = () => {
   return (
     <Routes>
         <Route path="/" element={<Index></Index>}></Route>
+        <Route path="/student/login" element={<StudentLogin></StudentLogin>}></Route>
+        <Route path="/recruiter/login" element={<RecruiterLogin></RecruiterLogin>}></Route>
+        <Route path="*" element={<Index></Index>}></Route>
     </Routes>
   )
 }
