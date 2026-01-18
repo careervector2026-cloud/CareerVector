6c72d48249d6826a6c649e4e8365a232fc0efd9f
Akshaykumar110904 <ravulakariakshaykumar@gmail.com>
2026-01-18 20:43:41 +0530

    added studentlogin,recruiterslogin,adminlogin pages

--- src/pages/Recruiters/RecruiterLogin.jsx
+++ src/pages/Recruiters/RecruiterLogin.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const RecruiterLogin = () => {
+export const RecruiterLogin = () => {
   return (
     <div>RecruiterLogin</div>
   )
 }
-
-export default RecruiterLogin
