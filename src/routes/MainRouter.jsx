b78cdd238a348508e2184a591a726d3bfa4eb3c3
Akshaykumar110904 <ravulakariakshaykumar@gmail.com>
2026-01-18 17:43:47 +0530

    Clear git cache to fix casing

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-import Index from "../pages/Index"
+import Index from "../pages/Index.jsx"
 const MainRouter = () => {
   return (
     <Routes>
