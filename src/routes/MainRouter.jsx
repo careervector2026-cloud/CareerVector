7304c527a9668e5ae2aa7d8752e229faefb340f4
Akshaykumar110904 <ravulakariakshaykumar@gmail.com>
2026-01-18 20:35:03 +0530

    added recruiters,students,admin folders

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -3,12 +3,14 @@ import { Routes,Route } from 'react-router-dom'
 import Index from "../pages/Index.jsx"
 import StudentLogin from "../pages/Students/StudentLogin.jsx"
 import RecruiterLogin from "../pages/Recruiters/RecruiterLogin.jsx"
+import AdminLogin from "../pages/Admins/AdminLogin.jsx"
 const MainRouter = () => {
   return (
     <Routes>
         <Route path="/" element={<Index></Index>}></Route>
         <Route path="/student/login" element={<StudentLogin></StudentLogin>}></Route>
         <Route path="/recruiter/login" element={<RecruiterLogin></RecruiterLogin>}></Route>
+        <Route path="/admin/login" element={<AdminLogin></AdminLogin>}></Route>
         <Route path="*" element={<Index></Index>}></Route>
     </Routes>
   )
