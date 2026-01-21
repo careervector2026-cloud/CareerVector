f19d510b92aa10dc2e3ba430a8d3076d0eff5da5
Pravalikareddy21 <pravalikareddy727@gmail.com>
2026-01-21 15:36:37 +0530

    create a  login and signup pages for students

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import { Routes,Route } from 'react-router-dom'
-import Index from "../pages/Index.jsx"
-import StudentLogin from "../pages/Students/StudentLogin.jsx"
-import RecruiterLogin from "../pages/Recruiters/RecruiterLogin.jsx"
-import AdminLogin from "../pages/Admins/AdminLogin.jsx"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Index from "../pages/Index.jsx";
+import StudentLogin from "../pages/Students/StudentLogin.jsx";
+import StudentSignup from "../pages/Students/studentsignup.jsx";
+import RecruiterLogin from "../pages/Recruiters/RecruiterLogin.jsx";
+import AdminLogin from "../pages/Admins/AdminLogin.jsx";
+
 const MainRouter = () => {
   return (
     <Routes>
-        <Route path="/" element={<Index></Index>}></Route>
-        <Route path="/student/login" element={<StudentLogin></StudentLogin>}></Route>
-        <Route path="/recruiter/login" element={<RecruiterLogin></RecruiterLogin>}></Route>
-        <Route path="/admin/login" element={<AdminLogin></AdminLogin>}></Route>
-        <Route path="*" element={<Index></Index>}></Route>
+      <Route path="/" element={<Index />} />
+      <Route path="/student/login" element={<StudentLogin />} />
+      <Route path="/student/signup" element={<StudentSignup />} />
+      <Route path="/recruiter/login" element={<RecruiterLogin />} />
+      <Route path="/admin/login" element={<AdminLogin />} />
+      <Route path="*" element={<Index />} />
     </Routes>
-  )
-}
-export default MainRouter
+  );
+};
+
+export default MainRouter;
