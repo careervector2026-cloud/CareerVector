0ba1516122eb05ce5224229a59d623162384731b
Katkuri Thrisha <katkurithrisha@gmail.com>
2026-01-21 16:12:14 +0530

    modified recruiters signup

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -7,12 +7,19 @@ import StudentSignup from "../pages/Students/studentsignup.jsx";
 import RecruiterLogin from "../pages/Recruiters/RecruiterLogin.jsx";
 import AdminLogin from "../pages/Admins/AdminLogin.jsx";
 
+import RecruiterSignup from "../pages/Recruiters/RecruiterSignup.jsx";
+import RecruiterVerify from "../pages/Recruiters/RecruiterVerify.jsx"; 
 const MainRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/student/login" element={<StudentLogin />} />
       <Route path="/student/signup" element={<StudentSignup />} />
+      <Route path="/recruiter/signup" element={<RecruiterSignup />} />
+
+        {/* ✅ ADDED ROUTE */}
+        <Route path="/recruiter/verify" element={<RecruiterVerify />} />
+
       <Route path="/recruiter/login" element={<RecruiterLogin />} />
       <Route path="/admin/login" element={<AdminLogin />} />
       <Route path="*" element={<Index />} />
